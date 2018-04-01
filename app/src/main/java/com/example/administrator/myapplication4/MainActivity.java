deea15ef0cb930bfba61e4ff56bdb3ec1a913d27
yukaihua <1376467150@qq.com>
2018-04-01 14:29:05 +0800

    qq

--- app/src/main/java/com/example/administrator/myapplication4/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication4/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.administrator.myapplication4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn:
+
+                break;
+        }
     }
 }
